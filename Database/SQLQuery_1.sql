bc6981f7c6caaa328a5966989407b5181cd6be49
phongcuncon <96615152+phongcuncon@users.noreply.github.com>
2022-11-18 08:53:12 +0700

    Thêm dữ liệu khách hàng

--- Database/SQLQuery_1.sql
+++ Database/SQLQuery_1.sql
@@ -1,4 +1,11 @@
 use QLKS_DB
 
-SELECT * from DichVu
+SELECT * from KhachHang
 
+insert into KhachHang VALUES('KH001',N'Nguyễn Thành Đạt',18,053203000732,N'Bạc',N'Tp Thủ Đức',N'0981263713',1,null);
+insert into KhachHang VALUES('KH013',N'Nguyễn Thị Anh',27,053195000412,N'Vàng',N'Long Khánh',N'0981296323',0,null);
+insert into KhachHang VALUES('KH041',N'Trần Kim Yến',30,053192073632,N'Vàng',N'Long An',N'0972113713',0,null);
+insert into KhachHang VALUES('KH051',N'Vũ Thị Thanh Như',61,053161053222,N'Kim cương',N'Tiền Ging',N'0981263713',0,null);
+insert into KhachHang VALUES('KH062',N'Trần Nhật Anh',22,053200003242,N'Vàng',N'Bến Tre',N'0983273413',1,null);
+insert into KhachHang VALUES('KH002',N'Lê Vũ Trung Kiên',26,053196061232,N'Bạc',N'Hậu Giang',N'0981127912',1,null);
+insert into KhachHang VALUES('KH005',N'Cảnh',24,053198064232,N'Bạc',N'Kiên Giang',N'0981263700',1,null);
