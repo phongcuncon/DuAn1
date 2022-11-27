d190a9408125250db6a034a0ce5884af0ea2aeee
phongcuncon <96615152+phongcuncon@users.noreply.github.com>
2022-11-27 13:54:16 +0700

    Form Cài Đặt

--- DauCungDuocHotel/src/UI/CaiDatJDialog.java
+++ DauCungDuocHotel/src/UI/CaiDatJDialog.java
@@ -27,46 +27,17 @@ public class CaiDatJDialog extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jLabel2 = new javax.swing.JLabel();
-
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-
-        jLabel1.setText("Cài đặt");
-
-        jButton1.setText("ádfsd");
-
-        jLabel2.setText("Cảnh ngu quá");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(324, 324, 324))
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(393, 393, 393)
-                        .addComponent(jLabel1))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(304, 304, 304)
-                        .addComponent(jLabel2)))
-                .addContainerGap(401, Short.MAX_VALUE))
+            .addGap(0, 836, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(103, 103, 103)
-                .addComponent(jButton1)
-                .addGap(109, 109, 109)
-                .addComponent(jLabel1)
-                .addGap(73, 73, 73)
-                .addComponent(jLabel2)
-                .addContainerGap(263, Short.MAX_VALUE))
+            .addGap(0, 605, Short.MAX_VALUE)
         );
 
         pack();
@@ -115,8 +86,5 @@ public class CaiDatJDialog extends javax.swing.JDialog {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
 }
