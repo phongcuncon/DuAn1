4205037bc87c9c9a1a5596f93115dad0035e6574
phongcuncon <96615152+phongcuncon@users.noreply.github.com>
2022-12-05 09:06:54 +0700

    thêm mới
    form Thống kê vẫn đang lỗi cùng với design lại form ChitietPhong

--- DauCungDuocHotel/src/DAO/ThongKeDAO.java
+++ DauCungDuocHotel/src/DAO/ThongKeDAO.java
@@ -8,7 +8,9 @@ import Entity.TKDichVu;
 import Entity.ThongKe;
 import Untils.JdbcHelper;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -64,5 +66,38 @@ public class ThongKeDAO {
 	String sql = "{CALL sp_ThongKeDoanhThu(?)}";
 	String[] cols = {"NgayXuat","Phong","TenKH","MaHD","DoanhThu","MaNV"};
 	return this.getListOfArray(sql, cols,nam);
+    }
+    private String SELECT_DOANH_THU = "SELECT SUM(THANHTOAN) FROM DONHANG WHERE YEAR(NGAYTAO)=? GROUP BY MONTH(NGAYTAO)";
+    private String THONG_KE_THEO_THOI_GIAN = "SELECT SUM(THANHTOAN) AS TONG, MIN(THANHTOAN) AS THAPNHAT, MAX(THANHTOAN) AS CAONHAT, AVG(THANHTOAN) AS TRUNGBINH FROM DONHANG WHERE NGAYTAO BETWEEN ? AND ?";
+
+    public List<Double> layDoanhThuTheoNam(int year) {
+        List<Double> list = new ArrayList<>();
+        try {
+            ResultSet rs = JdbcHelper.query(SELECT_DOANH_THU, year);
+            while (rs.next()) {
+                list.add(rs.getDouble(1));
+            }
+            rs.getStatement().getConnection().close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
+    }
+
+    public List<Double> thongKeDoanhThuTheoThoiGian(Date start, Date end) {
+        List<Double> list = new ArrayList<>();
+        try {
+            ResultSet rs = JdbcHelper.query(THONG_KE_THEO_THOI_GIAN, start, end);
+            while (rs.next()) {
+                list.add(rs.getDouble("Tong"));
+                list.add(rs.getDouble("CaoNhat"));
+                list.add(rs.getDouble("ThapNhat"));
+                list.add(rs.getDouble("TrungBinh"));
+            }
+            rs.getStatement().getConnection().close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
     }
 }
